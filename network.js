c12e30a201b63be20c69f778a4ad99ba745d4f8c
juanh2607 <jchernandez@fi.uba.ar>
2024-01-13 22:21:50 -0300

    Add mutation and more traffic

--- network.js
+++ network.js
@@ -28,6 +28,33 @@ class NeuralNetwork {
     
     return outputs;
   }
+
+  /**
+   * @param {NeuralNetwork} network - Foundation from which to mutate
+   * @param {number} amount - Amount of mutation. 1 = 100% (random) and 0 = exact replica
+   */
+  static mutate(network, amount = 1) {
+    network.levels.forEach(level => {
+      // Mutate biases
+      for (let i = 0; i < level.biases.length; i++) {
+        level.biases[i] = lerp(
+          level.biases[i],
+          Math.random()*2 - 1,
+          amount
+        )
+      }
+      // Mutate weights
+      for (let i = 0; i < level.weights.length; i++) {
+        for (let j = 0; j < level.weights[i].length; j++) {
+          level.weights[i][j] = lerp(
+            level.weights[i][j],
+            Math.random()*2 - 1,
+            amount
+          )
+        }
+      }
+    });
+  }
 }
 
 /**
